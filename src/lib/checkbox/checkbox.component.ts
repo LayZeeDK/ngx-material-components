098bdd22b42e8c83080cfc9686ae31e302051ed0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-06-03 05:06:26 +0200

    feat(checkbox): implement MDC adapter

--- src/lib/checkbox/checkbox.component.ts
+++ src/lib/checkbox/checkbox.component.ts
@@ -1,11 +1,18 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
+  ElementRef,
   OnDestroy,
   OnInit,
+  Renderer2,
+  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { MDCCheckboxAdapter, MDCCheckboxFoundation } from '@material/checkbox';
+
+import { as } from '../common/coercion';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,29 +20,99 @@ import { MDCCheckboxAdapter, MDCCheckboxFoundation } from '@material/checkbox';
   selector: 'mdc-checkbox',
   templateUrl: 'checkbox.component.html',
 })
-export class MdcCheckboxComponent implements OnInit, OnDestroy {
-  private readonly _adapter: Partial<MDCCheckboxAdapter> = {
-    addClass: (className: string): void => {},
-    deregisterAnimationEndHandler: (handler: EventListener): void => {},
-    deregisterChangeHandler: (handler: EventListener): void => {},
-    forceLayout: (): void => {},
-    getNativeControl: (): HTMLInputElement => document.createElement('input'),
-    isAttachedToDOM: (): boolean => true,
-    registerAnimationEndHandler: (handler: EventListener): void => {},
-    registerChangeHandler: (handler: EventListener): void => {},
-    removeClass: (className: string): void => {},
+export class MdcCheckboxComponent implements AfterViewInit, OnDestroy, OnInit {
+  private readonly adapter: MDCCheckboxAdapter = {
+    addClass: (className: string): void => {
+      this.renderer.addClass(this.control.nativeElement, className);
+    },
+    deregisterAnimationEndHandler: (handler: EventListener): void => {
+      if (!this.animationEndHandlers.has(handler)) {
+        return;
+      }
+
+      this.animationEndHandlers.get(handler)!();
+      this.animationEndHandlers.delete(handler);
+    },
+    deregisterChangeHandler: (handler: EventListener): void => {
+      if (!this.changeHandlers.has(handler)) {
+        return;
+      }
+
+      this.changeHandlers.get(handler)!();
+      this.changeHandlers.delete(handler);
+    },
+    /**
+     * Force-trigger a layout on the root element. This is needed to restart
+     * animations correctly. If you find that you do not need to do this, you
+     * can simply make it a no-op.
+     */
+    forceLayout: (): void => {
+      this.renderer.setProperty(
+        this.control.nativeElement,
+        'animation',
+        'none');
+      setTimeout(0, () => {
+        this.renderer.setProperty(
+          this.control.nativeElement,
+          'animation',
+          undefined
+        );
+        this.changeDetector.markForCheck();
+      });
+    },
+    getNativeControl: (): HTMLInputElement => !this.isAttachedToDom
+      ? as(undefined)
+      : this.control.nativeElement,
+    isAttachedToDOM: (): boolean => this.isAttachedToDom,
+    registerAnimationEndHandler: (handler: EventListener): void => {
+      this.changeHandlers.set(
+        handler,
+        this.renderer.listen(this.control.nativeElement, 'animationend', handler));
+    },
+    registerChangeHandler: (handler: EventListener): void => {
+      this.changeHandlers.set(
+        handler,
+        this.renderer.listen(this.control.nativeElement, 'change', handler));
+    },
+    removeClass: (className: string): void => {
+      this.renderer.removeClass(this.control.nativeElement, className);
+    },
+    removeNativeControlAttr: (attr: string): void => {
+      this.renderer.removeAttribute(this.control.nativeElement, attr);
+    },
+    setNativeControlAttr: (attr: string, value: string): void => {
+      this.renderer.setAttribute(this.control.nativeElement, attr, value);
+    },
   };
-  private get adapter(): MDCCheckboxAdapter {
-    return this._adapter as MDCCheckboxAdapter;
-  }
+  private readonly animationEndHandlers: Map<EventListener, (() => void)>
+    = new Map();
+  private readonly changeHandlers: Map<EventListener, (() => void)> = new Map();
   private foundation: MDCCheckboxFoundation =
     new MDCCheckboxFoundation(this.adapter);
+  private isAttachedToDom: boolean = false;
+
+  @ViewChild('control')
+  control!: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private readonly renderer: Renderer2,
+    private readonly changeDetector: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
     this.foundation.init();
   }
 
+  ngAfterViewInit(): void {
+    this.isAttachedToDom = true;
+  }
+
   ngOnDestroy(): void {
+    this.isAttachedToDom = false;
     this.foundation.destroy();
+    this.animationEndHandlers.forEach(deregisterHandler => deregisterHandler());
+    this.animationEndHandlers.clear();
+    this.changeHandlers.forEach(deregisterHandler => deregisterHandler());
+    this.changeHandlers.clear();
   }
 }
